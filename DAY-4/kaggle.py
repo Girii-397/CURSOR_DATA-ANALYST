87b5313ada10864e84ce9f56e217b7c7815183f8
GIRII-97 <giriprasadmungara97@gmail.com>
2024-11-21 19:32:05 +0530

    Third_commit

---
+++ DAY-4/kaggle.py
@@ -0,0 +1,6 @@
+import kagglehub
+ 
+# Download latest version
+path = kagglehub.dataset_download("iamtapendu/chest-x-ray-lungs-segmentation")
+ 
+print("Path to dataset files:", r'C:\Users\GiriprasadMungara\Tutorial\DAY-4\TEST.csv')
